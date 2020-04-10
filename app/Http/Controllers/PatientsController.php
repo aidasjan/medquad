feb97fb86afe2c368e43a3a0dcb9e25518ec472c
unknown <aidas.jankauskas31@gmail.com>
2020-04-10 19:35:31 +0300

    extended patient controller

--- app/Http/Controllers/PatientsController.php
+++ app/Http/Controllers/PatientsController.php
@@ -14,12 +14,27 @@ class PatientsController extends Controller
        return view('pages.patients.index')->with($patients);
    }
 
+   // GET
    public function createInit()
    {	
        return view('pages.patient.create_init');
    }
 
+   // POST
    public function storeInit(Request $request)
+   {	
+       dd($request->name);
+       return redirect('/patients/add/main');
+   }
+
+    // POST
+    public function createMain(Request $request)
+    {	
+        return view('pages.patient.create_main');
+    }
+
+   // POST
+   public function storeMain(Request $request)
    {	
        dd($request->all());
    }
