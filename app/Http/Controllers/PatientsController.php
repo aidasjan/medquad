076e6aa968e2061e1ee929bcdbe7e34e5ec06b4d
unknown <aidas.jankauskas31@gmail.com>
2020-04-10 19:42:03 +0300

    intendation fix

--- app/Http/Controllers/PatientsController.php
+++ app/Http/Controllers/PatientsController.php
@@ -8,24 +8,24 @@ use App\Patient;
 class PatientsController extends Controller
 {
 
-   public function index()
-   {
-       $patients = Patient::all();	
-       return view('pages.patients.index')->with($patients);
-   }
+    public function index()
+    {
+        $patients = Patient::all();	
+        return view('pages.patients.index')->with($patients);
+    }
 
-   // GET
-   public function createInit()
-   {	
-       return view('pages.patient.create_init');
-   }
+    // GET
+    public function createInit()
+    {	
+        return view('pages.patient.create_init');
+    }
 
-   // POST
-   public function storeInit(Request $request)
-   {	
-       dd($request->name);
-       return redirect('/patients/add/main');
-   }
+    // POST
+    public function storeInit(Request $request)
+    {	
+        dd($request->name);
+        return redirect('/patients/add/main');
+    }
 
     // POST
     public function createMain(Request $request)
@@ -33,11 +33,11 @@ class PatientsController extends Controller
         return view('pages.patient.create_main');
     }
 
-   // POST
-   public function storeMain(Request $request)
-   {	
-       dd($request->all());
-   }
+    // POST
+    public function storeMain(Request $request)
+    {	
+        dd($request->all());
+    }
 
 
 }
