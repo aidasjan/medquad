e88a863b99cbbc50d02340308a1af8d4d296efec
Antanas <antanas.ramana@gmail.com>
2020-04-10 20:03:05 +0300

    Migrations added

--- database/migrations/2020_04_10_145929_create_patients_table.php
+++ database/migrations/2020_04_10_145929_create_patients_table.php
@@ -17,7 +17,30 @@ class CreatePatientsTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('surname');
-            $table->integer('score');
+            $table->integer('age');
+            $table->string('group');
+            $table->boolean('cardiac_arrest');
+            $table->boolean('irreversible_hypotension');
+            
+            //Glasgow comma scale
+            $table->integer('best_motor_response')->nullable();
+            $table->integer('best_verbal_response')->nullable();
+            $table->integer('best_eye_response')->nullable();
+
+            $table->integer('burn_size')->nullable();
+            $table->boolean('other_mortality_conditions');
+
+            //Sofa scale
+            $table->double('pao2_fio2')->nullable();
+            $table->double('platelets')->nullable();
+            $table->double('bilirubin')->nullable(); // mg/dL are used in calculations
+            $table->double('mabp')->nullable();
+            $table->double('dopamine')->nullable();
+            $table->double('epinephrine')->nullable();
+            $table->double('norepinephrine')->nullable();
+            $table->double('creatinine')->nullable(); // mg/dL are used in calculations
+            $table->boolean('at_least_one_organ_failure')->nullable();
+
             $table->timestamps();
         });
     }
