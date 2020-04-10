33d602fab983b4b359ce0796bff3525a79c7572a
unknown <aidas.jankauskas31@gmail.com>
2020-04-10 17:46:26 +0300

    frontend init

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-dark navbar-laravel shadow-none container_main">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-uppercase" style='color: #fff;' href="#">Main</a>
+                </li>
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-3">
+                
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Page info -->
+    <title>FastB2B</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+
+    <link rel="shortcut icon" type="image/png" href="{{url('/favicon.png')}}"/>
+</head>
+<body>
+    <div id="app">
+        
+        @include('inc.navbar')
+        
+        <div class='text-center pt-5' style='padding-bottom: 12rem'>
+            @yield('content')
+        </div>
+
+    </div>
+</body>
+</html>
