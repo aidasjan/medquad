e1a1f2b313aa288680e38599331f697a9e25c5dc
unknown <aidas.jankauskas31@gmail.com>
2020-04-10 21:50:30 +0300

    add user

--- resources/views/pages/patient/create_main.blade.php
+++ resources/views/pages/patient/create_main.blade.php
@@ -6,7 +6,21 @@
 
         <div class='row container_white shadow py-4 my-3'>
             <div class='col'>
-
+                <div class='form-group col-md-10 offset-md-1'>
+                    <h3 class='my-4 font-weight-bold'>1. Glasgow comma scale</h3>
+                </div>
+                <div class='form-group col-md-3 offset-md-1'>
+                    <label>Best motor response</label>
+                    <input type='number' value="" name='best_motor_response' class='form-control' required>
+                </div>
+                <div class='form-group col-md-3 offset-md-1'>
+                    <label>Best motor response</label>
+                    <input type='number' value="" name='best_verbal_response' class='form-control' required>
+                </div>
+                <div class='form-group col-md-3 offset-md-1'>
+                    <label>Best motor response</label>
+                    <input type='number' value="" name='best_eye_response' class='form-control' required>
+                </div>
             </div>
         </div>
         
