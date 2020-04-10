020f248e4f0dd59f48da3bf695954268e95f3892
unknown <aidas.jankauskas31@gmail.com>
2020-04-11 00:54:08 +0300

    changed app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Page info -->
-    <title>FastB2B</title>
+    <title>Medquad</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
