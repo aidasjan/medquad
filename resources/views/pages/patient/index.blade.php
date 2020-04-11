f56eec1dd74008367414b11dbde49d688c17bdf7
unknown <aidas.jankauskas31@gmail.com>
2020-04-11 16:12:52 +0300

    disabled deleting

--- resources/views/pages/patient/index.blade.php
+++ resources/views/pages/patient/index.blade.php
@@ -45,7 +45,7 @@
                     <td style="font-weight:bold; text-align:left">{{$patient->surname}}</td>
                     <td style="font-weight:bold; text-align:left">{{$patient->age}}</td>
                     <td><a href='#'><i class="fas fa-edit" style="color:#bd0606" onclick="alert('Sorry, You are not allowed to perform this action')"></a></i></td>
-                    <td><a href='{{url("/patients/$patient->id/destroy")}}'><i class="fas fa-trash-alt" style="color:#bd0606"></a></i></td>
+                    <td><a href='#' onclick="alert('Sorry, You are not allowed to perform this action')"><i class="fas fa-trash-alt" style="color:#bd0606"></a></i></td>
                 </tr>
                 @endforeach
             <tbody>
